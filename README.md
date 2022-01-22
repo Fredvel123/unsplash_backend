35a39b61221df12b9244b151e61391adbd8a3bbe
Freddy <freddyvelarde59@gmail.com>
2022-01-21 21:38:49 -0400

    I uploaded the server to heroku cloud

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # UNSPLASH BACKEND 
 
 LINK FOR FRONTEND REPOSITORY https://github.com/Fredvel123/unsplash_frontend
+link for the server https://imagesfredvel.herokuapp.com/
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const app = require('./app');
 const PORT = app.get('port') 
+const HOST = ' https://imagesfredvel.herokuapp.com/'
 
 // data base mongodb config
 require('./database');
 
 app.listen(PORT, () => {
-  console.log(`the server is connected on port http://localhost:${PORT}`);
+  console.log(`the server is connected on port ${HOST}`);
 })
